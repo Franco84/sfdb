2268fc1b4b25a7ddfaa007b8090b67da89ed5e5d
franco83-2 <franco.84@gmail.com>
2017-11-14 23:09:48 -0800

    production csv location

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 require 'CSV'
 
-company_data = CSV.read 'files/companies.csv'
+company_file = File.join(Rails.root, 'files/score-companies.csv')
+company_data = CSV.read(company_file)
 headers = company_data.shift.map {|i| i.to_s }
 string_data = company_data.map {|row| row.map {|cell| cell.to_s } }
 company_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
 
-candidates = CSV.read('files/score-records.csv')
+candidate_file = File.join(Rails.root, 'files/score-records.csv')
+candidates = CSV.read(candidate_file)
 candidates.shift
 candidates.each do |candidate|
   fractal_index = company_hashes.select {|company| company['company_id'].to_i == candidate[4].to_i}

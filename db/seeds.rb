b508806de32e93ab72e0f73850e611b25f80b29d
franco83-2 <franco.84@gmail.com>
2017-11-14 23:23:55 -0800

    require csv to lowercase.. cmon heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
-company_data = CSV.read('./files/score-companies.csv')
+company_data = CSV.read(Rails.root + 'files/score-companies.csv')
 headers = company_data.shift.map {|i| i.to_s }
 string_data = company_data.map {|row| row.map {|cell| cell.to_s } }
 company_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
 
-candidates = CSV.read('./files/score-records.csv')
+candidates = CSV.read(Rails.root + 'files/score-records.csv')
 candidates.shift
 candidates.each do |candidate|
   fractal_index = company_hashes.select {|company| company['company_id'].to_i == candidate[4].to_i}
